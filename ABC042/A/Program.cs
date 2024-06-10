acaa8a92091e32011d7992c3e913a7afac38d184
navin-nawshin <navinmdnawshin@gmail.com>
2024-06-10 21:47:43 +0600

    first commit

---
+++ ABC042/A/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program{
+    public static void Main(String[] args){
+    var p = Console.ReadLine();
+    var num = p.Split().Select(int.Parse).ToList();
+    //splited the input into parts & turn them into int
+
+    if(num.Count(x => x==5) == 2 && num.Count(x => x==7) == 1){
+        //checking if list contains 2 '5' & 1 '7'
+        Console.WriteLine("YES");
+    }else{
+        Console.WriteLine("NO");
+    }
+    }
+}
