d274316c28572e7a7902d2b62189983822a0cafc
navin-nawshin <navinmdnawshin@gmail.com>
2024-07-09 01:37:02 +0600

    43 a

---
+++ ABC043/a/Program.cs
@@ -0,0 +1,11 @@
+﻿class Program{
+    public static void Main(string[] args){
+        Console.Write("N: ");
+        int c = Convert.ToInt32(Console.ReadLine());
+        int t=0;
+
+        for(int i=0; i<=c;i++){
+            t = t+i;
+        }Console.Write($"{t} candies to the child!");
+    }
+}
