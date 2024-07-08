04ada55f10d7693cf9e6c577f7e153c6fdc3d344
navin-nawshin <navinmdnawshin@gmail.com>
2024-07-09 01:08:05 +0600

    c add

--- ABC042/A/Program.cs
+++ ABC042/A/Program.cs
@@ -1,14 +1,14 @@
 ﻿class Program{
     public static void Main(String[] args){
+    Console.WriteLine("Input the int in this manner: A B C");
+    Console.Write("Input: ");
     var p = Console.ReadLine();
     var num = p.Split().Select(int.Parse).ToList();
     //splited the input into parts & turn them into int
 
-    if(num.Count(x => x==5) == 2 && num.Count(x => x==7) == 1){
-        //checking if list contains 2 '5' & 1 '7'
-        Console.WriteLine("YES");
-    }else{
-        Console.WriteLine("NO");
-    }
+    var c = num.Count(x => x==5) == 2 && num.Count(x => x==7) == 1 ? "Yes" : "No";
+    //checking if list contains 2 '5' & 1 '7'
+
+    Console.WriteLine(c);
     }
 }
