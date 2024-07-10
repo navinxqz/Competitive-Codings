7fe2354a19420ad0e1d7aab9e78b009f9aa5f47b
navin-nawshin <navinmdnawshin@gmail.com>
2024-07-10 17:05:01 +0600

    fix

---
+++ ABC043/b/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program{
+    public static string Key(string key){
+        string String = "";
+
+        foreach(char c in key){
+            switch(c){
+                case '0': String += c; break;
+                case '1': String += c; break;
+                case 'B': if(String.Length > 0){ //for backspace key
+                    String = String.Substring(0, String.Length-1);
+                }break;
+                default: Console.WriteLine("[s consists of 0, 1 & B!]"); break;
+            }   
+            /*  if(c == '0' || c=='1'){ String += c; }
+                else if(c=='B'){
+                    if(String.Length > 0){ //for backspace key
+                    String = String.Substring(0, String.Length-1);
+                }}else{Console.WriteLine("[s consists of 0, 1 & B!]");} 
+*/
+        }return String;
+    }
+    public static void Main(String[] args){
+        Console.Write("Input: ");
+        string s = Console.ReadLine();
+        Console.WriteLine("Output: "+ Key(s));
+    }
+}
