fd5fb647f3f355423a1c6ea255bccf4ec23ab55b
navin-nawshin <navinmdnawshin@gmail.com>
2024-07-11 15:39:35 +0600

    init B

---
+++ ABC044/B/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program{
+    public static bool IsBeautiful(string s){
+        if(s==null){return true;}
+    }
+    //var counter = new Dictionary<char, int>();
+    int[] counter = new int[26];    //for 26 letters
+    foreach (char c in s)
+    {
+        counter[c - 'a']++;
+    }foreach(int count in counter){
+        bool check = (count%2 !=0) ? false : true;
+    }
+
+    public static void Main(string[] args){
+        System.Console.Write("Input String: ");
+        string input = Console.ReadLine();
+
+        Console.WriteLine("String beautiful? : {0}",check);
+    }
+}
