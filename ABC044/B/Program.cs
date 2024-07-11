f38e2d4103195dace0141b90aebe4855568cdec3
navin-nawshin <navinmdnawshin@gmail.com>
2024-07-11 16:01:00 +0600

    array for letters

--- ABC044/B/Program.cs
+++ ABC044/B/Program.cs
@@ -1,20 +1,20 @@
 ﻿class Program{
     public static bool IsBeautiful(string s){
         if(s==null){return true;}
-    }
-    //var counter = new Dictionary<char, int>();
-    int[] counter = new int[26];    //for 26 letters
-    foreach (char c in s)
-    {
-        counter[c - 'a']++;
-    }foreach(int count in counter){
-        bool check = (count%2 !=0) ? false : true;
-    }
 
+    int[] counter = new int[26];    //for 26 letters
+    foreach (char c in s){
+        counter[c - 'a']++;
+    }bool check = true;
+
+    foreach(int count in counter){  //check for the even num
+        check = (count%2 !=0) ? false : check;
+    }return check;
+    }
     public static void Main(string[] args){
-        System.Console.Write("Input String: ");
+        Console.Write("Input String: ");
         string input = Console.ReadLine();
 
-        Console.WriteLine("String beautiful? : {0}",check);
+        Console.WriteLine("String beautiful? : {0}",IsBeautiful(input));
     }
 }
