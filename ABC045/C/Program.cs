30c352ce27f3c19426e9bfa329adce3d4a0d356e
navin-nawshin <navinmdnawshin@gmail.com>
2024-07-13 15:36:26 +0600

    divider added

--- ABC045/C/Program.cs
+++ ABC045/C/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿//using System.Collections.Generic;
 class Program{
     static long Temp(string s){ //used long to store large value that int can't handle
     //to sum the part separated by +
@@ -10,21 +10,23 @@ class Program{
             output += long.Parse(e2);
         }return output;
     }
-    static int Combination(int n){
+    static int Combination(int n){  //recursive method
         if(n==0){ return 1;}
         return 2*Combination(n-1);
     }
     static string[] forms(string s){    //all the possible formula & insert +
         int combination = Combination(s.Length-1);
         string[] result = new string[combination];
+
         for(int i = 0; i<combination; i++){
             string pattern = "";
-
+            int div=1;
             for(int j=0;j<s.Length;j++){
                 pattern += s[j];
-                if(j<s.Length-1 && ((i >> j)& 1)== 1){
+
+                if(j<s.Length-1 && (i/div)%2 == 1){
                     pattern += "+";
-                }
+                }div *=2;
             }result[i] = pattern;
         }return result;
     }
