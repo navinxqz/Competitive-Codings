26c205019ddaa41cb72345ae78d58b9612493f12
navin-nawshin <navinmdnawshin@gmail.com>
2024-07-13 14:24:11 +0600

    store the value by separation

--- ABC045/C/Program.cs
+++ ABC045/C/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program{
+    static long Temp(string s){ //used long to store large value that int can't handle
+        long output = 0;
+        long temp = 0;
+
+        string[] e = s.Split('+');
+        foreach(string e2 in e){
+            temp = temp*10 + int.Parse(e2);
+        }output += temp;
+        return output;
+    }
+    public static void Main(string[] args){}
+}
