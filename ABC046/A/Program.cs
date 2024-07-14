983a8148166db0cc31e4cf6b7ce50df8e318b640
navin-nawshin <navinmdnawshin@gmail.com>
2024-07-14 23:10:48 +0600

    more simplier

--- ABC046/A/Program.cs
+++ ABC046/A/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System.Linq;
 class Program{
     public static void Main(string[] args){
-        string[] s = Console.ReadLine().Split(' '); //split the input into ary
+    /*    string[] s = Console.ReadLine().Split(' '); //split the input into ary
         int a = int.Parse(s[0]);    //parse string to int
         int b = int.Parse(s[1]);
         int c = int.Parse(s[2]);
 
         int[] color = {a,b,c};
-        Console.WriteLine(color.Distinct().Count());
+        Console.WriteLine(color.Distinct().Count());    */
+        Console.WriteLine(Console.ReadLine().Split(' ').Distinct().Count());
     }
 }
